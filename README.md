ade6b01c54c89ab8125a15b07723aca0a498f61e
Chinedu Egbulefu <egbulefuchinedu@gmail.com>
2023-05-19 16:48:06 +0400

    finished Inception

--- README.md
+++
@@ -1,2 +0,0 @@
-# Inception
-This project aims to broaden your knowledge of system administration by using Docker.
--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
-wp --allow-root --path=/var/www core install --url="cegbulef.42.fr" --title="Chinedu's Word-Press" --admin_user=wpaduser --admin_password=$WPADPASS --admin_email="cegbulef@student.42abudhabi.ae"
+wp --allow-root --path=/var/www core install \
+  --url="$DOMAIN_NAME" \
+  --title="Chinedu's Word-Press" \
+  --admin_user="$WPADUSER" \
+  --admin_password="$WPADPASS" \
+  --admin_email="cegbulef@student.42abudhabi.ae"
 
-wp user create wpuser1 "user1@student.42abudhabi.ae" --role=author --user_pass=$WPUSER1PASS --allow-root
+wp user create "$WPUSER1" "user1@student.42abudhabi.ae" \
+  --role=author \
+  --user_pass="$WPUSER1PASS" \
+  --allow-root
 
 wp --allow-root --path=/var/www option update blogname "Chinedu's Word-Press"
 wp --allow-root --path=/var/www option update blogdescription "The Inception of Things"
 wp --allow-root --path=/var/www option update blog_public 0
 
-wp theme install salient --activate --allow-root
+# REDIS CACHE
+wp config set WP_CACHE true --allow-root
+wp config set WP_CACHE_KEY_SALT 'cegbulef.42.fr' --allow-root
 
 wp plugin install redis-cache --activate --allow-root
+wp redis enable --allow-root
+
+wp theme install twentytwentyone --activate --allow-root
 
 wp plugin update --all --allow-root
 
